9702602b92fc89c27e1e47dbf22ce03ab73918d7
purple-mountain <lmwrong92@gmail.com>
2023-06-17 15:40:19 +0500

    Deleted error in health check

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dis'))
 
 app.get('/version', (req, res) => {
-  res.send('1.2') // change this string to ensure a new version deployed
+  res.send('1.3') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
